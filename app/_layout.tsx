4c153475e6ee86edd0fcf42eb5e03085c7a98fce
romanshrestha20 <stha.roman20@gmail.com>
2026-01-07 01:08:12 +0200

    feat: implement RootLayout with AuthProvider and UserProvider for context management

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,11 @@
-import { Stack } from "expo-router";
+import { AuthProvider } from "@/src/features/auth/auth.context.js";
+import { UserProvider } from "@/src/features/user/user.context.js";
+import { ReactNode } from "react";
 
-export default function RootLayout() {
-  return <Stack />;
+export default function RootLayout({ children }: { children: ReactNode }) {
+  return (
+    <AuthProvider>
+      <UserProvider>{children}</UserProvider>
+    </AuthProvider>
+  );
 }
